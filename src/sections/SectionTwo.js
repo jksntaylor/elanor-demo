052faa709c7856e1debbc201eafe5a0161b71e3e
jksntaylor <josephjacksontaylor@gmail.com>
2021-02-16 19:08:57 -0700

    this kinda works? I hate it though

--- src/sections/SectionTwo.js
+++ src/sections/SectionTwo.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import { gsap } from 'gsap'
 
 import ArrowLeft from '../assets/images/arrow-left.svg'
@@ -33,22 +34,65 @@ const SectionTwo = props => {
   const [currentCar, setCurrentCar] = useState('landRover')
 
   const [animationRan, setAnimationRan] = useState(false)
+  const [transitionRunning, setTransitionRunning] = useState(false)
 
-  const handleClick = () => setCurrentCar(currentCar === 'landRover' ? 'porsche' : 'landRover')
+  // this is ugly code I'm aware, but it works.
+  const handleClick = () => {
+    if (transitionRunning) return
+    else setTransitionRunning(true)
+
+    if (currentCar === 'landRover') {
+      document.getElementById('landRover-info').className = 'car-info leaving'
+      document.getElementById('porsche-info').className = 'car-info entering'
+      document.getElementById('landRover-image').className = 'car-image leaving'
+      document.getElementById('porsche-image').className = 'car-image entering'
+      setTimeout(() => {
+        document.getElementById('landRover-info').className = 'car-info hidden'
+        document.getElementById('porsche-info').className = 'car-info visible'
+        document.getElementById('landRover-image').className = 'car-image hidden'
+        document.getElementById('porsche-image').className = 'car-image visible'
+      }, 500);
+      setTimeout(() => {
+        setTransitionRunning(false)
+      }, 600);
+    }
+    else {
+      document.getElementById('landRover-info').className = 'car-info entering'
+      document.getElementById('porsche-info').className = 'car-info leaving'
+      document.getElementById('landRover-image').className = 'car-image entering'
+      document.getElementById('porsche-image').className = 'car-image leaving'
+      setTimeout(() => {
+      document.getElementById('landRover-info').className = 'car-info visible'
+      document.getElementById('porsche-info').className = 'car-info hidden'
+      document.getElementById('landRover-image').className = 'car-image visible'
+      document.getElementById('porsche-image').className = 'car-image hidden'
+      }, 500);
+      setTimeout(() => {
+        setTransitionRunning(false)
+      }, 600);
+    }
+    setCurrentCar(currentCar === 'landRover' ? 'porsche' : 'landRover')
+  }
 
   const [sectionRef, inView] = useInView({threshold: 0.6});
   // animation references
   const carouselRef = useRef()
-  const carRef = useRef()
   const titleRef = useRef()
   const paragraphRef = useRef()
   const carouselContentRef = useRef()
 
+  // transition references
+  const landRoverInfoRef = useRef()
+  const landRoverImageRef = useRef()
+  const porscheInfoRef = useRef()
+  const porscheImageRef = useRef()
+
   useEffect(() => {
     if (!props.isMobile && inView && !animationRan) {
       // animations
+      gsap.to([landRoverInfoRef.current], {opacity: 1, duration: 0.1})
       gsap.to([carouselRef.current], {scaleX: 1})
-      gsap.to([carRef.current], {x: 0, opacity: 1, delay: 0.2})
+      gsap.to([landRoverImageRef.current], {x: 0, opacity: 1, delay: 0.2})
       gsap.to([titleRef.current], {y: 0, opacity: 1, delay: 0.2})
       gsap.to([paragraphRef.current], {y: 0, opacity: 1, delay: 0.5})
       gsap.to([carouselContentRef.current], {opacity: 1, delay: 0.6})
@@ -64,27 +108,49 @@ const SectionTwo = props => {
               <img src={ArrowLeft} alt="Arrow Left" onClick={handleClick}/>
               <img src={ArrowRight} alt="Arrow Right" onClick={handleClick}/>
             </div>
-            <div className="car-info">
-              <h1>{carData[currentCar].make}</h1>
-              <h2>{carData[currentCar].model}</h2>
-              <div className="mpg">
-                <img src={Pump} alt="Gas Pump"/>
-                <h4>MPG</h4>
-                <h3>{carData[currentCar].mpg}</h3>
+            <div className="car-info-transition-container">
+              <div id="landRover-info" className="car-info visible" ref={landRoverInfoRef}>
+                <h1>{carData.landRover.make}</h1>
+                <h2>{carData.landRover.model}</h2>
+                <div className="mpg">
+                  <img src={Pump} alt="Gas Pump"/>
+                  <h4>MPG</h4>
+                  <h3>{carData.landRover.mpg}</h3>
+                </div>
+                <div className="hp">
+                  <img src={Graph} alt="Line Graph"/>
+                  <h4>HP</h4>
+                  <h3>{carData.landRover.hp}</h3>
+                </div>
+                <div className="zeroSixty">
+                  <img src={Gauge} alt="Speedometer"/>
+                  <h4>0-60</h4>
+                  <h3>{carData.landRover.zeroSixty}</h3>
+                </div>
               </div>
-              <div className="hp">
-                <img src={Graph} alt="Line Graph"/>
-                <h4>HP</h4>
-                <h3>{carData[currentCar].hp}</h3>
-              </div>
-              <div className="zeroSixty">
-                <img src={Gauge} alt="Speedometer"/>
-                <h4>0-60</h4>
-                <h3>{carData[currentCar].zeroSixty}</h3>
+              <div id="porsche-info" className="car-info hidden" ref={porscheInfoRef}>
+                <h1>{carData.porsche.make}</h1>
+                <h2>{carData.porsche.model}</h2>
+                <div className="mpg">
+                  <img src={Pump} alt="Gas Pump"/>
+                  <h4>MPG</h4>
+                  <h3>{carData.porsche.mpg}</h3>
+                </div>
+                <div className="hp">
+                  <img src={Graph} alt="Line Graph"/>
+                  <h4>HP</h4>
+                  <h3>{carData.porsche.hp}</h3>
+                </div>
+                <div className="zeroSixty">
+                  <img src={Gauge} alt="Speedometer"/>
+                  <h4>0-60</h4>
+                  <h3>{carData.porsche.zeroSixty}</h3>
+                </div>
               </div>
             </div>
           </div>
-          <img className="car-image" src={carData[currentCar].img} alt="car" ref={carRef}/>
+          <img id="landRover-image" className="car-image" src={carData.landRover.img} alt="car" ref={landRoverImageRef}/>
+          <img id="porsche-image" className="car-image" src={carData.porsche.img} alt="car" ref={porscheImageRef}/>
         </div>
         <div className="text">
           <h2 ref={titleRef}>SELECT A VEHICLE FROM YOUR PHONE.</h2>
