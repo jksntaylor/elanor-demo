021b3c52f3b19c4c9217113016cb6eb9b1740e7e
jksntaylor <josephjacksontaylor@gmail.com>
2021-02-16 20:36:34 -0700

    loading "page"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 import SectionOne from './sections/SectionOne'
 import SectionTwo from './sections/SectionTwo'
@@ -7,14 +7,21 @@ import SectionFour from './sections/SectionFour'
 import SectionFive from './sections/SectionFive'
 import SectionSix from './sections/SectionSix'
 import Footer from './sections/Footer'
+import Loader from './sections/Loader'
 
 import './assets/styles/Main.css'
 
 const App = () => {
   let isMobile = window.innerWidth < 1024 ? true : false
+  let [pageLoaded, setPageLoaded] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoaded(true)
+    }, 1000);
+  })
   return (
     <div className="App">
-      {/* LOADING PAGE */}
+      {pageLoaded ?
       <main id="lander">
         <SectionOne isMobile={isMobile}/>
         <SectionTwo isMobile={isMobile} />
@@ -24,6 +31,9 @@ const App = () => {
         <SectionSix isMobile={isMobile} />
         <Footer />
       </main>
+      :
+      <Loader />
+      }
     </div>
   );
 }
--- src/sections/Loader.js
+++ src/sections/Loader.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Icon from '../assets/images/logo-icon.svg'
 
 export default function Loader() {
   return (
     <section id="loader">
-
+      <div className="icon-container">
+        <div className="circle"/>
+      </div>
+      <img className="icon" src={Icon} alt="Logo"/>
     </section>
   )
 }
